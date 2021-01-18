2ccb787f739902ee369df4e4a2efde3296030296
Chris Hesterman <chrishesterman14@gmail.com>
2021-01-18 10:01:59 -0800

    Adjust display styling

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,18 +4,29 @@ import styled from 'styled-components';
 const DisplayStyled = styled.div`
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   width: 22rem;
+  height: 3rem;
   border: 1px solid black;
   text-transform: uppercase;
   background: black;
   color: red;
+  font-size: 1.5rem;
   margin-bottom: 25px;
+  padding: 0 20px;
+  overflow-x: hidden;
+`;
+
+const PStyled = styled.p`
+  margin-top: 0;
+  margin-bottom: 0;
+  text-align: middle;
 `;
 
 const Display = (props) => {
   return (
     <DisplayStyled>
-      <p>{props.readout}</p>
+      <PStyled>{props.readout}</PStyled>
     </DisplayStyled>
   );
 };
